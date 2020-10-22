9fdd8baf07ebe3fd82b40cf55943dbbed21050fd
AbsoluteVirtueXI <absolutevirtuexi@gmail.com>
2020-10-22 11:39:29 +0200

    typo

--- exercices/ethereum/correction-exercices-soutien.md/TheOne.sol
+++ exercices/ethereum/correction-exercices-soutien.md/TheOne.sol
@@ -5,7 +5,8 @@ contract TheOne {
   // Quel est le type de mes variables ?
   // Quel est le type des arguments et de la valeur de retour de mes fonctions ?
   // Est ce que ma fonction peut recevoir de l'ether ? (payable ou pas ?)
-  // Quel est la visibilité de mes variables et de mes fonctions ? (rien, view ou pure ?)
+  // Quel est l'impact de ma fonction sur l'état du smart contract?
+  // écrit/lit des variables ou aucuns des 2?  (rien, view ou pure )
   function one() public pure returns (uint256) {
     return 1;
   }
