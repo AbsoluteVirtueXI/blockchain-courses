04a84d16869bb32ab7e51ec65ab5094ec30c0131
AbsoluteVirtueXI <absolutevirtuexi@gmail.com>
2020-10-23 11:06:08 +0200

    exo

---
+++ exercices/ethereum/correction-exercices-smart-contracts/TransactionCounter.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+contract TransactionCounter {
+  uint256 private _counter;
+  address private _owner;
+  mapping(address => bool) private _addresses;
+
+  constructor() public {
+    _counter = 0;
+    _owner = msg.sender;
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == _owner, 'Only admin can perform this action');
+    _;
+  }
+
+  modifier onlyAddress() {
+    require(_addresses[msg.sender], 'Not allowed');
+    _;
+  }
+
+  function addTicker(address _address) public onlyOwner {
+    _addresses[_address] = true;
+  }
+
+  function delTicker(address _address) public onlyOwner {
+    _addresses[_address] = false;
+  }
+
+  function isTicker(address _address) public view returns (bool) {
+    return _addresses[_address];
+  }
+
+  function tick() public onlyAddress returns (uint256) {
+    _counter += 1;
+    return _counter;
+  }
+
+  function counter() public view returns (uint256) {
+    return _counter;
+  }
+}
