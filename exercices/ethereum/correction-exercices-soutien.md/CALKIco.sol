8da8b266276d31109f454be623c514511b99ebbc
AbsoluteVirtueXI <absolutevirtuexi@gmail.com>
2020-10-26 08:01:00 +0100

    ico generic

--- exercices/ethereum/correction-exercices-soutien.md/CALKIco.sol
+++ exercices/ethereum/correction-exercices-soutien.md/CALKIco.sol
@@ -2,20 +2,22 @@
 pragma solidity ^0.6.0;
 import './CalcToken.sol';
 
-// CalcToken deployed at 0xc311D39FbD47F122ba89Cb4EBdB9b06bc3deEf77
+// CalcToken deployed at 0x3e34cf7C6347dA5CBa804188075F8D5CC6B6A73f
 // price = 100000000000000000 wei d'ether (0.1 ether)
-// CALKIco deployed at  0x4E5b37D822AF3527159d493744954Fa77eBED911
+// CALKIco deployed at  0x83508Ca7F827c526DF8993f1d3b35880B9C6767B
 contract CALKIco {
   // Declare a FirstErc20 contract
   FirstErc20 public token;
 
-  // The price of 1 unit of our token in wei;
+  // The price of 1 token in wei;
   uint256 public _price;
 
   // Address of token seller
   address payable private _seller;
 
   uint256 private _ratio;
+
+  uint256 private _decimal;
 
   constructor(
     uint256 price, // price pour 1 CALK
@@ -25,25 +27,48 @@ contract CALKIco {
     _price = price;
     _seller = seller;
     token = FirstErc20(erc20Address);
-    _ratio = (10**uint256(token.decimals())) / _price; // 1 token / prix d'un token = ratio
+    _decimal = (10**uint256(token.decimals()));
+  }
+
+  function getCurrentPrice() public view returns (uint256) {
+    return _price;
+  }
+
+  function getPricePerNbTokens(uint256 nbTokens) public view returns (uint256) {
+    uint256 buyPrice = (nbTokens * _price) / _decimal;
+    require(buyPrice > 0, 'CALKIco: Need a higher number of tokens');
+    return buyPrice;
+  }
+
+  function getNbTokensPerPrice(uint256 _buyPrice)
+    public
+    view
+    returns (uint256)
+  {
+    uint256 nbTokens = (_buyPrice * _decimal) / _price;
+    require(
+      nbTokens > 0,
+      'CALKIco: Need a higher amount of ether for buying tokens'
+    );
+    return nbTokens;
   }
 
   receive() external payable {
-    buy(msg.value * _ratio);
+    buy(getNbTokensPerPrice(msg.value));
   }
 
   // nbTokens en wei de CALK
   function buy(uint256 nbTokens) public payable returns (bool) {
-    // check is ether > 0
-    require(msg.value > 0, 'ICO: price minimum 1 wei');
-    // check is nbTokens superieur au mini qui 10 wei token
-    require(nbTokens >= 10, 'ICO: minimum 10 wei tokens');
-    // check si montant est OK
+    // check if ether > 0
+    require(msg.value > 0, 'ICO: purchase price can not be 0');
+    // check if nbTokens > 0
+    require(nbTokens > 0, 'ICO: Can not purchase 0 tokens');
+    // check if enough ethers for nbTokens
     require(
-      (nbTokens * _price) / (10**uint256(token.decimals())) <= msg.value,
-      'ICO: Not enough Ether for purchase'
+      msg.value >= getPricePerNbTokens(nbTokens),
+      'ICO: Not enough ethers for purchase'
     );
-    uint256 _realPrice = (nbTokens * _price) / (10**uint256(token.decimals()));
+    uint256 _realPrice = getPricePerNbTokens(nbTokens);
     uint256 _remaining = msg.value - _realPrice;
     token.transferFrom(_seller, msg.sender, nbTokens);
     _seller.transfer(_realPrice);
